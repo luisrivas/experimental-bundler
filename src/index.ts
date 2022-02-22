25df8d8d395a8acceda0b13d2b8046e7982f6aef
Oscar Plaza <oscar.plaza@optimizely.com>
2022-02-22 12:27:49 -0500

    Add plugin options

--- src/index.ts
+++ src/index.ts
@@ -15,40 +15,24 @@ async function main() {
     .action(async (files, options) => {
       console.log(files, options)
       const esbuild = require('esbuild');
-      const { swcPlugin } = require('esbuild-plugin-swc');
+      const { swcPlugin } = require('./plugin');
+      const sourceMap = true // options
 
-
-      esbuild.build({
+      let result = await esbuild.build({
         entryPoints: files,
         bundle: true,
         outdir: options.d || 'lib',
-        resolveExtensions: ['.mjs','.js'],
-        plugins: [
-          swcPlugin({
-            "jsc": {
-              "parser": {
-                "syntax": "ecmascript",
-                "jsx": false,
-                "dynamicImport": false,
-                "privateMethod": false,
-                "functionBind": false,
-                "exportDefaultFrom": false,
-                "exportNamespaceFrom": false,
-                "decorators": false,
-                "decoratorsBeforeExport": false,
-                "topLevelAwait": false,
-                "importMeta": false
-              },
-              "transform": null,
-              "target": "es2015",
-              "loose": false,
-              "externalHelpers": false,
-              // Requires v1.2.50 or upper and requires target to be es2016 or upper.
-              "keepClassNames": false
-            }
-          })
-        ]
-      })
+        resolveExtensions: ['.ts', '.mjs','.js'],
+        minify: true,
+        treeShaking: true,
+        metafile: true,
+        logLevel: 'verbose',
+        sourcemap: sourceMap,
+        plugins: [swcPlugin('es5', sourceMap)]
+      });
+
+      let text = await esbuild.analyzeMetafile(result.metafile)
+      console.log(text)
     })
 
   cli.help()
---
+++ src/plugin.ts
@@ -0,0 +1,48 @@
+import { Options as SWCOptions, JscTarget,  Output, transform, transformSync,  } from '@swc/core';
+import { Plugin, OnLoadArgs, OnLoadResult, OnResolveArgs } from 'esbuild';
+import path from 'path';
+import fs from 'fs/promises';
+
+const judgeTS = (p: string): boolean => p.endsWith('.ts') || p.endsWith('.tsx');
+
+export function swcPlugin(target: JscTarget = 'es2015', sourcemap:boolean = false): Plugin {
+  return {
+    name: 'esbuild:swc',
+    setup(builder) {
+      builder.onResolve({ filter: /\.([cm]?[tj]sx?)$/ }, (args: OnResolveArgs) => {
+        const fullPath = path.resolve(args.resolveDir, args.path);
+        return {
+          path: fullPath
+        }
+      });
+
+      builder.onLoad({ filter: /\.([cm]?[tj]sx?)$/ }, async (args: OnLoadArgs): Promise<OnLoadResult> => {
+        const code = await fs.readFile(args.path, 'utf-8');
+        const isTS = judgeTS(args.path);
+        const isJSX = args.path.endsWith('x');
+
+        const initialOptions: SWCOptions = {
+          jsc: {
+            parser: {
+              syntax: isTS ? 'typescript' : 'ecmascript',
+              ...(isTS && isJSX ? { tsx: true } : {}),
+              ...(!isTS && isJSX ? { jsx: true } : {}),
+            },
+            target: target,
+          },
+          filename: args.path,
+          sourceMaps: sourcemap ? 'inline': false,
+          sourceFileName: args.path
+        };
+        
+        let result: Output;
+        result = await transform(code, initialOptions);
+
+        return {
+          contents: result.code,
+          loader: 'js'
+        }
+      })
+    }
+  }
+}
